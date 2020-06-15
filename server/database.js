f790d551de0b01931bd897f305225e3204bd766f
heegupark <slhead@gmail.com>
2020-06-14 20:40:34 -0700

    changed url to live server

--- server/database.js
+++ server/database.js
@@ -17,7 +17,7 @@ db.connect(err => {
     console.error('DB connection error', err.stack);
   } else {
     // eslint-disable-next-line no-console
-    console.log('DB connection is successful');
+    console.log('DB connection is successful.');
   }
 });
 module.exports = db;
