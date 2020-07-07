6a25da97f7e3c6fd8d3d071de6316936cf44ce4a
heegupark <slhead@gmail.com>
2020-07-06 17:45:00 -0700

    fixed minor bugs

--- client/components/disclaimer.jsx
+++ client/components/disclaimer.jsx
@@ -17,7 +17,7 @@ class Disclaimer extends Component {
       <div className="modal">
         <div className="modal-content">
           <p className="mx-auto my-3 h4">Welcome to Omega Fresh</p>
-          <div className="mx-auto my-2 text-center">This app is created strictly for demonstration purposes. By clicking the button below, you accept that no purchases will be made, no payment processing will be done, and actual personal information should not be used in checkout.</div>
+          <div className="mx-3 my-2 text-center">This app is created strictly for demonstration purposes. By clicking the button below, you accept that no purchases will be made, no payment processing will be done, and actual personal information should not be used in checkout.</div>
           <button className="btn btn-sm btn-danger mx-auto my-3" onClick={handleAcceptClick}>Accept</button>
         </div>
       </div>
